f030dc632ce579505585c5cb382727f3aa7edb2c
rajafrom26 <pulir941@gmail.com>
2025-12-03 15:06:48 +0530

    remove unnecessary

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -4,17 +4,6 @@ const ProductDetail = () => {
 
   return (
     <div className='container w-50'>
-        <div className="row">
-            <div className="col-lg-5">
-                <h1>
-                    hello world
-                </h1>
-            </div>
-            <div className="col-lg-7">
-                <h1>we are Reading</h1>
-            </div>
-        </div>
-      
     </div>
   )
 }
